f3d7ba57c71852eb65339a44ae58f5ef0ed51fdb
Nick Brown <nickrossbrown@gmail.com>
2018-10-29 22:07:18 -0700

    controller  for season weeks added with html pages

---
+++ app/controllers/weeks_controller.rb
@@ -0,0 +1,60 @@
+class WeeksController < ApplicationController
+
+  def new
+    @season = Season.find(params[:season_id])
+    @week = @season.weeks.new  
+  end
+
+  def index
+    @season = Season.find(params[:season_id])
+    @weeks = @Season.weeks.all
+  end
+
+  def show
+    @season = Season.find(params[:season_id])
+    @week = @season.weeks.find(params[:id])
+  end
+
+
+  def create
+    @Season = Season.find(params[:season_id])
+    @week = @season.weeks.new(week_params)
+    if @week.save
+      redirect_to season_path(@week.season)
+      flash[:notice] = "week successfully added!"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @Season = Season.find(params[:season_id])
+    @week = @Season.weeks.find(params[:id])
+    @week.destroy
+    flash[:notice] = "week successfully destroyed!"
+    redirect_to food_path(@week.food)
+  end
+
+  def edit
+    @Season = Season.find(params[:season_id])
+    @week = @Season.weeks.find(params[:id])
+  end
+
+  def update
+    @Season = Season.find(params[:season_id])
+    @week = @Season.weeks.find(params[:id])
+    # binding.pry
+    if @week.update(week_params)
+      flash[:notice] = "week successfully updated!"
+      redirect_to food_path(@week.food)
+    else
+      render :edit
+    end
+  end
+
+private
+  def week_params
+    params.require(:week).permit(:id, :season_id, :description)
+  end
+
+end
